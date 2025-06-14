690a45c9beaf327b419bb7183127390f733f9ee9
TriNM <nguyentri16112003@gmail.com>
2025-06-14 10:52:06 +0700

    feat: add routes, context, types

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { ThemeProvider } from "@/components/theme/theme-provider"
 import { ModeToggle } from "./components/ui/mode-toggle"
+import AppRoutes from "./routes"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <ModeToggle/>
-      </div>
+      <AppRoutes/>
     </ThemeProvider>
   )
 }
---
+++ app/src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PublicRoutes from "./PublicRoutes";
+import PrivateRoutes from "./PrivateRoutes";
+
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      {PublicRoutes()}
+
+      <Route element={<PrivateRoutes />}>
+   
+      </Route>
+
+      {/* 404 fallback */}
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
